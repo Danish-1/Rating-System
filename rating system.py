ac0f88f7ee85cb699b41f8befdb9919031019980
Danish <57083546+Danish-1@users.noreply.github.com>
2020-12-31 00:12:56 +0530

    My commit

---
+++ rating system.py
@@ -0,0 +1,17 @@
+b = 0   # no. of reviews initially 0
+c = 0   # sum of all rating initially 0
+# infinite loop
+while True:
+	# take input
+	a = int(input("\n Give your Rating under 1 to 5 : ")) 
+	# validate input
+	if a in range(1, 6):
+		b += 1				     # increment reviews
+		d = (a + c)/b         #  calculate rating 
+		# print rating and reviews
+		print("\n Rating☆ : ", d,"\t  reviews : ", b)
+		# sum of all rating
+		c += a
+	# Invalide input
+	else:
+		print("\n please rate under 1 to 5")
